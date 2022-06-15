218d4f3905301030c4fb7a8f818c3143f80f3dd2
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-15 12:20:24 +0900

    Update ProgC3.java

--- src/main/java/ProgC3.java
+++ src/main/java/ProgC3.java
@@ -2,7 +2,6 @@ public class ProgC3 {
 
     public static void main(String[] args) {
         DNA dna = new DNA();
-        //dna.str = "CTCGAGAGTATATGTTGTTGAACGTTATTGTTTGAGAAATAGTTGATGCATCAGAATGGTTTGCATTTAT";
         dna.str = "AGCTCGAATATGC";
         System.out.println(dna.getPair());
         System.out.println(dna.getLevenshteinDistance("ACTGAAGAGC"));
