7713513a2e929eab15ec6aea0651a1fecfe6c7ff
Dai Hasegawa <dhasegawa@hgu.jp>
2022-07-07 10:58:46 +0900

    fix psudo code and tests

--- src/main/java/ProgC3.java
+++ src/main/java/ProgC3.java
@@ -3,7 +3,6 @@ public class ProgC3 {
     public static void main(String[] args) {
         DNA dna = new DNA();
         dna.str = "AGCTCGAATATGC";
-        System.out.println(dna.getPair());
         System.out.println(dna.getLevenshteinDistance("ACTGAAGAGC"));
     }
 }
--- src/test/java/TestDNA.java
+++ src/test/java/TestDNA.java
@@ -40,11 +40,12 @@ public class TestDNA {
         // assertion
         String[] prints = bos.toString().split("\r\n|\n", -1);
         try {
-            assertEquals(" 0  1  2  3  4  5 ", prints[0],"int型二次元配列dの初期化が疑似コードと異なります!");
-            assertEquals(" 1  0  0  0  0  0 ", prints[1],"int型二次元配列dの初期化が疑似コードと異なります!");
-            assertEquals(" 2  0  0  0  0  0 ", prints[2],"int型二次元配列dの初期化が疑似コードと異なります!");
-            assertEquals(" 3  0  0  0  0  0 ", prints[3],"int型二次元配列dの初期化が疑似コードと異なります!");
-            assertEquals("", prints[4],"int型二次元配列dの初期化表示の直下に改行以外の文字があります!");
+            assertEquals(" 0  1  2  3  4  5  6", prints[0],"int型二次元配列dの初期化が疑似コードと異なります!");
+            assertEquals(" 1  0  0  0  0  0  0", prints[1],"int型二次元配列dの初期化が疑似コードと異なります!");
+            assertEquals(" 2  0  0  0  0  0  0", prints[2],"int型二次元配列dの初期化が疑似コードと異なります!");
+            assertEquals(" 3  0  0  0  0  0  0", prints[3],"int型二次元配列dの初期化が疑似コードと異なります!");
+            assertEquals(" 4  0  0  0  0  0  0", prints[4],"int型二次元配列dの初期化が疑似コードと異なります!");
+            assertEquals("", prints[5],"int型二次元配列dの初期化表示の直下に改行以外の文字があります!");
         } catch (ArrayIndexOutOfBoundsException excpt) {
                 fail("DNA.getLevenshteinDistance()のprint出力が不足しています!");
         }
@@ -70,10 +71,11 @@ public class TestDNA {
         // assertion
         String[] prints = bos.toString().split("\r\n|\n", -1);
         try {
-            assertEquals(" 0  1  2  3  4  5 ", prints[5],"d[i][j]の計算に間違いがあります!");
-            assertEquals(" 1  0  1  2  3  4 ", prints[6],"d[i][j]の計算に間違いがあります!");
-            assertEquals(" 2  1  1  2  2  3 ", prints[7],"d[i][j]の計算に間違いがあります!");
-            assertEquals(" 3  2  2  1  2  3 ", prints[8],"d[i][j]の計算に間違いがあります!");
+            assertEquals(" 0  1  2  3  4  5  6", prints[6],"d[i][j]の計算に間違いがあります!");
+            assertEquals(" 1  0  1  2  3  4  5", prints[7],"d[i][j]の計算に間違いがあります!");
+            assertEquals(" 2  1  0  1  2  3  4", prints[8],"d[i][j]の計算に間違いがあります!");
+            assertEquals(" 3  2  1  1  2  2  3", prints[9],"d[i][j]の計算に間違いがあります!");
+            assertEquals(" 4  3  2  2  1  2  3", prints[10],"d[i][j]の計算に間違いがあります!");
         } catch (ArrayIndexOutOfBoundsException excpt) {
                 fail("DNA.getLevenshteinDistance()のprint出力が不足しています!");
         }
